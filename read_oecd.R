d39748ac60ee2580507bf4281faa3022d7c87b65
gdolfus <gdolfus@gmail.com>
2013-11-08 18:59:12 +0200

    first commit

---
+++ read_oecd.R
@@ -0,0 +1,14 @@
+# 	Gero Dolfus
+# 	University of Helsinki, HECER
+# 	Start: November 08, 2013.
+#
+#	First look at trade statistics from OECD.
+#
+#	2-digits only.
+#
+#	Read the data.
+#
+#	http://unstats.un.org/unsd/tradekb/Knowledgebase/Harmonized-Commodity-Description-and-Coding-Systems-HS
+#
+#
+#
