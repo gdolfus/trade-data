1a1614a3c2a70946dc6cfd3af4ecb423af151072
gdolfus <gdolfus@gmail.com>
2014-05-14 13:19:03 +0300

    loop over countries

--- read_feenstra_raw.R
+++ read_feenstra_raw.R
@@ -44,112 +44,85 @@ dirname.data = "~/RRR_finn/data/feenstra/"
 # - - - - - - - - - - - - - - - - - - - - - - 
 
 
+countries = c("Fm USSR", "World", "UK", "Germany", "USA", 
+	"Sweden", "Norway", "France,Monac", "Denmark")
 
+label = countries
+tmp.short = c("su", "wrld", "uk", "ger", "us", "swe", 
+	"nor", "fra", "den")
+label = data.frame(label, tmp.short)
+names(label)[1] = "orig"
+names(label)[2] = "short"
 
+rm(list = ls(pattern = "tmp"))
+
+# "Fm USSR" is not in the data past 1991.
+years = 75:91
 
 # - - - - - - - - - - - - - - - - - - - - - -  
 #
-# 		Finland - USSR
+# 		Finland - XXX
 #
 # - - - - - - - - - - - - - - - - - - - - - - 
 
-years = 75:95
 
 tmp.old <- NULL
 
-for (i in years) {
+for (k in countries) {
 
-	# Read the data.
-	tmp.dat <- read.dta(paste(dirname.data, "/wtf", i, "/wtf", 
-		i, ".dta", sep = ""))
-	tmp.dat <- tmp.dat[tmp.dat$exporter == "Finland" & tmp.dat$importer == 
-		"Fm USSR", ]
-	tmp.dat$importer = "USSR"
-	
-	# Remove columns that I don't need.
-	tmp.dat <- tmp.dat[, -match(c("icode", "ecode", "unit", "dot", 
-		"quantity"), names(tmp.dat))]
+	tmp.label = label$short[which(label$orig == k)]
+
+	for (i in years) {
+
+		# Read the data.
+		tmp.dat <- read.dta(paste(dirname.data, "/wtf", 
+			i, "/wtf", i, ".dta", sep = ""))
+		tmp.dat <- tmp.dat[tmp.dat$exporter == "Finland" & 
+			tmp.dat$importer == k, ]
+		# Rename USSR for consistency with other scripts.
+		if (k == "Fm USSR") {
+			tmp.dat$importer = "USSR"
+		}
+
+		# Remove columns that I don't need.
+		tmp.dat <- tmp.dat[, -match(c("icode", "ecode", 
+			"unit", "dot", "quantity"), names(tmp.dat))]
+
+		# 		tmp.dat <- tmp.dat[, -match(c("icode", "ecode", 
+		# "dot"), names(tmp.dat))]
 
 
 
 
+		if (length(tmp.dat$sitc4) - length(unique(tmp.dat$sitc4)) != 
+			0) {
+			print(paste("Same codes used more than once in", 
+				i, unique(tmp.dat$importer)))
 
-	if (length(tmp.dat$sitc4) - length(unique(tmp.dat$sitc4)) != 
-		0) {
-		print(paste("Same codes used more than once in", i, unique(tmp.dat$importer)))
 
+		}
+		tmp.old = rbind(tmp.old, tmp.dat)
 
 	}
-		tmp.old = rbind(tmp.old, tmp.dat)
-	
+
+
+
+	# Housekeeping.
+	write.table(tmp.old, paste(dirname.data, "fin-ex-", 
+		tmp.label, "-panel-raw.csv", sep = ""), row.names = F, 
+		sep = ",")
+
 }
-
-print(paste('Year=',i,'that is, the USSR no longer exists.'))
-
-
-# Housekeeping.
-write.table(tmp.old, paste(dirname.data, "fin-ex-su-panel-raw.csv", 
-	sep = ""), row.names = F, sep = ",")
 
 
 # "Fm USSR" is not in the data past 1991.
-tmp.dat <- read.dta(paste(dirname.data, "/wtf", 92, "/wtf", 92, 
-	".dta", sep = ""))
-tmp.dat[tmp.dat$exporter == "Finland" & tmp.dat$importer == "Fm USSR", 
-	]
+# tmp.dat <- read.dta(paste(dirname.data, "/wtf", 92, "/wtf", 92, 
+# ".dta", sep = ""))
+# tmp.dat[tmp.dat$exporter == "Finland" & tmp.dat$importer == "Fm USSR", 
+# ]
 # If I want to use that, I will have to aggregate it somehow.
 
-rm(list = ls(pattern = "tmp"),i)
-
-
-
-
-
-
-# - - - - - - - - - - - - - - - - - - - - - -  
-#
-# 		Finland - World
-#
-# - - - - - - - - - - - - - - - - - - - - - - 
-
-years = 75:95
-
-tmp.old <- NULL
-
-for (i in years) {
-
-	# Read the data.
-	tmp.dat <- read.dta(paste(dirname.data, "/wtf", i, "/wtf", 
-		i, ".dta", sep = ""))
-	tmp.dat <- tmp.dat[tmp.dat$exporter == "Finland" & tmp.dat$importer == 
-		"World", ]
-	# Remove columns that I don't need.
-	tmp.dat <- tmp.dat[, -match(c("icode", "ecode", "unit", "dot", 
-		"quantity"), names(tmp.dat))]
-
-	if (length(tmp.dat$sitc4) - length(unique(tmp.dat$sitc4)) != 
-		0) {
-		# Aggregate the data to unique SITC codes.		
-		tmp.df <- aggregate(tmp.dat$value, by = list(tmp.dat$sitc4), 
-			FUN = sum)
-		tmp.dat <- tmp.dat[match(tmp.df$Group.1, tmp.dat$sitc4), 
-			]
-		tmp.dat$value <- tmp.df$x
-
-	}
-
-
-		# Combine it with the data for earlier years.
-	tmp.old = rbind(tmp.old, tmp.dat)
-
-}
-
-
-# Housekeeping.
-write.table(tmp.old, paste(dirname.data, "fin-ex-world-panel-raw.csv", 
-	sep = ""), row.names = F, sep = ",")
-
-rm(list = ls(pattern = "tmp"))
+rm(list = ls(pattern = "tmp"), i)
 
 
 
